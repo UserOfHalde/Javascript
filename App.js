2c57356b9c6c2f050c1e3f877e4f9e72f8d9380f
UserOfHalde <waldemar.ei@gmx.net>
2017-06-27 19:27:44 +0200

    an IFFE not able to go _ not IFFE

--- App.js
+++ App.js
@@ -1,3 +1,28 @@
 function IIFE() {
+    let zauber = 0;
+    let kraft = 100;
+    let mächte = 0;
 
-}
+
+    let fülleAlles = function() {
+        this.zauber = 100;
+        this.kraft = 100;
+        this.mächte = "allmächtig";
+    }
+
+    let zustandAnzeige = () => {
+        console.log('\n' +
+            this.zauber +
+            '\n' +
+            this.kraft +
+            '\n' +
+            this.mächte);
+    }
+
+    return zustandAnzeige;
+
+};
+
+let zauberer = IIFE();
+
+zauberer.zustandAnzeige();
