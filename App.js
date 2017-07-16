7819b0512b76856ad0b7ab1aa4e8f38525b3a57c
UserOfHalde <waldemar.ei@gmx.net>
2017-07-16 16:15:31 +0200

    gestestet: filter(func) : die filter-Funktion bekommt eine Funktion als Argument übergeben, diese Funktion kann nun alles Mögliche anstellen  mit dem von Filter jeweils angeschauten Element, das sich in dem Array befindet welches wiederum  die Filtermethode aufruft/anwendet.

---
+++ App.js
@@ -0,0 +1,11 @@
+var ages = [32, 33, 16, 40];
+
+function checkAdult(age) {
+    return age >= 18;
+}
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = ages.filter(checkAdult);
+}
+
+console.log(ages.filter(checkAdult));
